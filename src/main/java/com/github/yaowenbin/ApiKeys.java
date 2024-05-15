c35064967bc0251ef121d8ff8e40feece98b43dc
yao-wenbin <ywb992134@163.com>
2024-05-15 14:42:38 +0800

    feat: add API Key from System.getenv()

--- src/main/java/com/github/yaowenbin/ApiKeys.java
+++ src/main/java/com/github/yaowenbin/ApiKeys.java
@@ -3,5 +3,9 @@ package com.github.yaowenbin;
 public class ApiKeys {
 
     public static final String OPENAI_API_KEY = "demo";
+    public static final String QIANFAN_API_KEY = System.getenv("QIANFAN_API_KEY");
+    public static final String QIANFAN_SECRET_KEY = System.getenv("QIANFAN_SECRET_KEY");
+
+
 }
 
