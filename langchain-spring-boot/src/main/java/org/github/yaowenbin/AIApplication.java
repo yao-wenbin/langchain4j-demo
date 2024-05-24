147564522c6514913056a90063b2d8e95df77186
yao-wenbin <ywb992134@163.com>
2024-05-24 11:04:46 +0800

    feat: Spring Boot Integration

---
+++ langchain-spring-boot/src/main/java/org/github/yaowenbin/AIApplication.java
@@ -0,0 +1,12 @@
+package org.github.yaowenbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AIApplication {
+    public static void main(String... args) {
+        SpringApplication.run(AIApplication.class, args);
+    }
+}
